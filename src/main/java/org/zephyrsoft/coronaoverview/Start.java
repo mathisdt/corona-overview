9d52ca6c4fc5fe4ce5545b794d9caa8fc8d08820
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2021-04-17 20:34:39 +0200

    make history invisible at page load (can be displayed with a click on "..."); add statement about refresh time and source of the raw data; add i18n

--- src/main/java/org/zephyrsoft/coronaoverview/Start.java
+++ src/main/java/org/zephyrsoft/coronaoverview/Start.java
@@ -10,10 +10,12 @@ import java.net.URL;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.zephyrsoft.coronaoverview.model.CsvEntry;
@@ -25,6 +27,7 @@ import com.opencsv.bean.CsvToBeanBuilder;
 public class Start {
 
 	private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("EEEE, dd.MM.yyyy");
+	private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("EEEE, dd.MM.yyyy, HH:mm");
 	private static final NumberFormat NUMBER_FORMAT = DecimalFormat.getNumberInstance();
 	static {
 		NUMBER_FORMAT.setMinimumFractionDigits(1);
@@ -62,35 +65,67 @@ public class Start {
 					groupingBy(CsvEntry::getMeldedatum,
 						mapping(CsvEntry::getSiebenTagesInzidenzPro100000Einwohner, MoreCollectors.onlyElement()))));
 
+			ResourceBundle strings = ResourceBundle.getBundle("strings");
+
 			StringBuilder output = new StringBuilder(
-				"<html><head><title>Corona-Overview</title><meta charset=\"UTF-8\"></head><body>\n");
+				"<html>\n<head><title>Corona-Overview</title><meta charset=\"UTF-8\"><link rel=\"icon\" href=\"favicon.ico\">"
+					+ "<script type=\"text/javascript\">function details(classname) {\n"
+					+ " var el = document.getElementsByClassName(classname + \"m\");\n"
+					+ " for (var i = 0; i < el.length; i++) {\n"
+					+ "  el[i].style.display = \"none\";\n"
+					+ " }\n"
+					+ " el = document.getElementsByClassName(classname);\n"
+					+ " for (var i = 0; i < el.length; i++) {\n"
+					+ "  el[i].style.display = \"inherit\";\n"
+					+ " }\n"
+					+ "}</script>"
+					+ "</head>\n<body style=\"padding:30px;padding-left:60px\">\n");
 			for (String location : locations) {
 				result.entrySet().stream()
 					.filter(e -> e.getKey().startsWith(location))
 					.forEach(perLocation -> {
 						output.append("<h3>")
 							.append(perLocation.getKey())
-							.append("</h3>\n<table>\n");
+							.append("</h3>\n<table style=\"padding-left:20px\">\n");
 
+						String locationClass=location.replaceAll("[^a-zA-Z]", "").toUpperCase();
 						AtomicInteger line = new AtomicInteger();
 						perLocation.getValue().entrySet().stream()
 							.sorted(Comparator.comparing((Map.Entry<LocalDate, Double> e) -> e.getKey()).reversed())
 							.limit(5)
 							.forEach(perDay -> {
-								output.append("<tr><td align=\"right\">")
+								output.append("<tr")
+									.append(line.get() > 0 ? " class=\"" + locationClass + "\" style=\"display:none\"" : " style=\"display:inherit\"")
+									.append("><td align=\"right\">")
 									.append(line.get() > 0 ? "" : "<b>")
 									.append(NUMBER_FORMAT.format(perDay.getValue()))
 									.append(line.get() > 0 ? "" : "</b>")
-									.append("</td><td style=\"padding-left:10px\">(")
+									.append("</td><td style=\"padding-left:10px;color:#606060\"><small>(")
 									.append(DATE_FORMAT.format(perDay.getKey()))
-									.append(")</td></tr>\n");
+									.append(")</small></td></tr>\n");
+								if (line.get() == 0) {
+									output.append("<tr class=\"")
+										.append(locationClass)
+										.append("m\" onclick=\"details('")
+										.append(locationClass)
+										.append("')\"><td colspan=\"2\" style=\"text-align:center\">...</td></tr>\n");
+								}
 								line.incrementAndGet();
 							});
 
 						output.append("</table>\n");
 					});
 			}
-			output.append("</body></html>");
+			output
+				.append("<br/><br/><br/><small><i>")
+				.append(strings.getString("data_from"))
+				.append(" ")
+				.append(DATE_TIME_FORMAT.format(LocalDateTime.now()))
+				.append(" ")
+				.append(strings.getString("oclock"))
+				.append("<br/>\n")
+				.append(strings.getString("data_source"))
+				.append(" <a href=\"https://www.niedersachsen.de/Coronavirus/aktuelle_lage_in_niedersachsen/\" target=\"_blank\">niedersachsen.de</a></i></small></body>\n</html>");
 
 			System.out.println(output.toString());
 
